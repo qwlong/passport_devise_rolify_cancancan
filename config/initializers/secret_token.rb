aa3e9d24d586032b8bfa8754c55a91872658bd1c
qwlong <wenlongqi@gmail.com>
2014-05-01 16:24:09 +0800

    initialize permissions

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Passport::Application.config.secret_key_base = 'a1ff53c6e5c1137231d212016aec7b157516da275be33bf42ae8185ad27a73124e5bead4706a6c925d68273312aa421d35234da8262e68a618a05d327466f459'
